c4e6a9c2e9691995314f61e02b61fd10799d65d8
joseamorone <jose.amorone@gmail.com>
2021-03-23 10:29:44 -0300

    corrections done

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -176,7 +176,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  Return Math.random ();
+  return Math.random ();
 }
 
 function esPositivo(numero) {
